fdd2c6d0b773f24c63c2f57a5bc47b8e05c3c438
Dale Sande <dale.sande@gmail.com>
2012-02-18 10:16:44 -0800

    building new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UXMad::Application.config.secret_token = 'd06d3ca9be46490ff7de431e760ade3e6dfe934722ccca566e4ca0cad3340011e0b129e13b597a820cbfe7bce831a103e8a5eedc3bfe8c6dfaa92cbb136aa550'
