bf9ea8198a0beab797deb515144e70d4140376e3
Matt Gauger <matt.gauger@gmail.com>
2012-04-17 19:30:20 -0500

    Use the correct API key

--- config/initializers/copycopter.rb
+++ config/initializers/copycopter.rb
@@ -1,4 +1,4 @@
 CopycopterClient.configure do |config|
-  config.api_key = '4e7a091342594b6f2865f7592e05707d'
+  config.api_key = '6beaf0b8811c5d743299c20c316961ff'
   config.host = 'sapling-events-copy.herokuapp.com'
 end
