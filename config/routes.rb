e17e4dc7a942e4be98fbf091b71d635070788133
Jim Remsik <jim@bendyworks.com>
2012-11-19 22:22:26 -0600

    Dyanicizing home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,26 +7,8 @@ SnowMobile::Application.routes.draw do
   resources :speakers, :only => [:index]
   resources :sponsors, :only => [:index]
 
-#  get "/speakers" => 'home#speakers'
-#  get "/sponsors" => 'home#sponsors'
-#  get "/schedule" => 'home#schedule'
-#  get "/travel" => 'home#travel'
+  get "/lodging" => 'home#lodging', :as => 'lodging'
   get "/register" => 'home#register'
-#  get "/about" => 'home#about'
-#  get "/contact" => 'home#contact'
 
-  get "/expect" => 'home#expect'
   root :to => "home#home"
-
-  get "/axle" => "axle#grids"
-  get "/axle/text" => "axle#text"
-  get "/axle/abstract_colors" => "axle#abstract_colors"
-  get "/axle/semantic_colors" => "axle#semantic_colors"
-  get "/axle/widgets" => "axle#widgets"
-  get "/axle/forms" => "axle#forms"
-  get "/axle/buttons" => "axle#buttons"
-
-  get "/axle/widgets_site_header" => "axle#widgets_site_header"
-  get "/axle/widgets_home_blocks" => "axle#widgets_home_blocks"
-
 end
