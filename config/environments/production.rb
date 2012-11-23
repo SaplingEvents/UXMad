0ba8e50ed6bf2b2bc8c9028f5eb444b8ae876464
Jim Remsik <jim@bendyworks.com>
2012-11-23 11:50:26 -0600

    style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ SnowMobile::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
- 
+
   # Mailer settings
   config.action_mailer.default_url_options = { :host => 'uxmad.com' }
 
